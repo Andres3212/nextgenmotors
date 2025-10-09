b78ca3029fb67ec865a9361853768c1058f159a8
andres <andresandrade3212@gmail.com>
2025-10-08 21:45:27 -0500

    init commit

--- src/main/java/org/example/nextgenmotors2/NextGenMotors2Application.java
+++ src/main/java/org/example/nextgenmotors2/NextGenMotors2Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.swing.*;
-
+//Hola
 @SpringBootApplication
 public class NextGenMotors2Application implements ApplicationRunner {
 
